e55a47b7176cd1eff051d6147554e9e813b21960
SuchanyaSd <suchanya_sd@hotmail.com>
2025-01-21 13:53:26 +0700

    new

--- src/components/PageNavigate.jsx
+++ src/components/PageNavigate.jsx
@@ -2,18 +2,38 @@ import React from "react";
 
 function PageNavigate (props) {
     const {currentPage, setCurrentPage, total} = props
+
+    const start = (currentPage-1)*10+1
+    const end = currentPage*10
+
+    const hdlClick = (n) => {
+        if (currentPage+n < 1) {
+            return 
+        }
+        setCurrentPage(prv => +prv+n)
+    }
+
+    const hdlChange = (e) => {
+        if (isNaN(e.target.value) || e.target.value.trim() === '') {
+            return setCurrentPage(prv => prv)
+        }
+        setCurrentPage(e.target.value)
+    }
+
     return (
         <div className="flex gap-1 border">
             <button
-             className="bg-pink-300 border p-1">
+             className="bg-pink-300 border p-1"
+             onClick={()=>{hdlClick(-1)}}
+             >
                 Prev
             </button>
             <input className="w-10" 
                 value={currentPage}
-                onChange={e => setCurrentPage(e.target.value)}
+                onChange={hdlChange}
             />
-            <button className="bg-pink-300 border p-1">Next</button>
-            <p>1 -10 of {total}</p>
+            <button className="bg-pink-300 border p-1" onClick={()=>{hdlClick(1)}}>Next</button>
+            <p>{start}-{end} of {total}</p>
         </div>
     )
 }
