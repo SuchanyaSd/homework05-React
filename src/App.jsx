5e2d03d5243ed33b3067f580f466a0a00df5d2e1
SuchanyaSd <suchanya_sd@hotmail.com>
2025-01-21 10:45:39 +0700

    mock ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import PageNavigate from "./components/PageNavigate";
+import ProductItem from "./components/ProductItem";
+import ProductList from "./components/ProductList";
+import SearchBar from "./components/SearchBar";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-pink-500 hover:underline">Hello world!</h1>
-    </>
+    <div className="bg-slate-400 max-w-screen-sm m-5 p-3">
+      <h1 className="text-3xl font-bold text-pink-500 hover:underline">
+        Product Search
+      </h1>
+      <div className="flex gap-4">
+        <SearchBar/>
+        <PageNavigate/>
+      </div>
+      <ProductList/>
+    </div>
   );
 }
 
---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductItem() {
+  return (
+    <div>
+      ProductItem
+    </div>
+  )
+}
+
+export default ProductItem
